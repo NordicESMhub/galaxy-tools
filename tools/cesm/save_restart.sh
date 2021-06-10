356e52c9d0102b3ad32a287bc226a415dc955b8c
Anne Fouilloux <annefou@geo.uio.no>
2021-06-10 19:25:21 +0000

    make sure we can use restart files

---
+++ tools/cesm/save_restart.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+WORKDIR=$1
+CASENAME=$2
+RESDIR=$3
+INFO_FILE=$4
+
+echo "===================================================================="
+echo "                Save restarts for CESM run                          " 
+echo "===================================================================="
+
+nb_nc=$(find $WORKDIR -type f -name "${CASENAME}.*.r*.*.nc" | wc -l)
+nb_bin=$(find $WORKDIR -type f -name "${CASENAME}.*.r*.*.bin" | wc -l)
+nb_i=$(find $WORKDIR -type f -name "${CASENAME}.*.i*.*.bin" | wc -l)
+nb_rp=$(find $WORKDIR -type f -name "rpointer.*" | wc -l)
+    
+if [[ $nb_nc -gt 0 ]]; then 
+    cp $WORKDIR/${CASENAME}.*.r*.*.nc $RESDIR/ 2>>$INFO_FILE
+fi
+if [[ $nb_bin -gt 0 ]]; then 
+    cp $WORKDIR/${CASENAME}.*.r*.*.bin $RESDIR/ 2>>$INFO_FILE
+fi
+if [[ $nb_i -gt 0 ]]; then 
+    cp $WORKDIR/${CASENAME}.*.i*.*.bin $RESDIR/ 2>>$INFO_FILE
+fi
+if [[ $nb_rp -gt 0 ]]; then 
+    cp $WORKDIR/rpointer.* $RESDIR/ 2>>$INFO_FILE
+fi
+echo "Restart saved."
