81f56e80811be547f0dffc048b801ab049ad03d1
Anne Fouilloux <annefou@geo.uio.no>
2021-06-11 18:24:42 +0000

    fix namelist names

--- tools/cesm/save_restart.sh
+++ tools/cesm/save_restart.sh
@@ -11,7 +11,7 @@ echo "===================================================================="
 
 nb_nc=$(find $WORKDIR -type f -name "${CASENAME}.*.r*.*.nc" | wc -l)
 nb_bin=$(find $WORKDIR -type f -name "${CASENAME}.*.r*.*.bin" | wc -l)
-nb_i=$(find $WORKDIR -type f -name "${CASENAME}.*.i*.*.bin" | wc -l)
+nb_i=$(find $WORKDIR -type f -name "${CASENAME}.*.i*.*.nc" | wc -l)
 nb_rp=$(find $WORKDIR -type f -name "rpointer.*" | wc -l)
     
 if [[ $nb_nc -gt 0 ]]; then 
@@ -21,7 +21,7 @@ if [[ $nb_bin -gt 0 ]]; then
     cp $WORKDIR/${CASENAME}.*.r*.*.bin $RESDIR/ 2>>$INFO_FILE
 fi
 if [[ $nb_i -gt 0 ]]; then 
-    cp $WORKDIR/${CASENAME}.*.i*.*.bin $RESDIR/ 2>>$INFO_FILE
+    cp $WORKDIR/${CASENAME}.*.i*.*.nc $RESDIR/ 2>>$INFO_FILE
 fi
 if [[ $nb_rp -gt 0 ]]; then 
     cp $WORKDIR/rpointer.* $RESDIR/ 2>>$INFO_FILE
