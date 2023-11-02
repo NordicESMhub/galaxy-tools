33c2de6aecf217839af53cc7c0e43e9187d70068
Anne Fouilloux <annef@simula.no>
2023-11-02 15:22:56 +0100

    draft netcdf2zarr tool

---
+++ tools/netcdf2zarr/xarray_netcdf2zarr.py
@@ -0,0 +1,19 @@
+import argparse
+import ast
+import warnings
+
+import cftime  # noqa: F401
+
+import xarray as xr  # noqa: I202,E402
+
+import zarr # noqa: I202,E402
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", type=str, help="input file in netCDF format")
+parser.add_argument("-o", "--output", type=str, help="output in Zarr format")
+args = parser.parse_args()
+
+dset = xr.open_dataset(args.input)
+
+dset.to_zarr(args.output)
+dset.close()
