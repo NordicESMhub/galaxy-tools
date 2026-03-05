85ddf7bd324a0d68f27100cc7497bf06fae5bab0
PaulineSGN <107857379+PaulineSGN@users.noreply.github.com>
2026-03-05 15:21:45 +0100

    Modify file checks to handle multiple archive formats

--- tools/cads/cads.sh
+++ tools/cads/cads.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -f download.tar.gz ]; then
-   tar zxvf  download.tar.gz
+if [ -f *.tar.gz ]; then
+   tar zxvf  *.tar.gz
    cat *.grib > tmp.grib
    cdo setgridtype,regular tmp.grib tmpg.grib
    cdo -f nc -t ecmwf copy tmpg.grib tmp.nc
-elif [ -f download.zip ]; then
-   unzip download.zip
+elif [ -f *.zip ]; then
+   unzip *.zip
    cat *.grib > tmp.grib
    cdo setgridtype,regular tmp.grib tmpg.grib
    cdo -f nc -t ecmwf copy tmpg.grib tmp.nc
-elif [ -f download.grib ]; then
-   cdo -f nc -t ecmwf copy download.grib tmp.nc
-elif [ -f download.nc ]; then
-   mv download.nc tmp.nc
+elif [ -f *.grib ]; then
+   cdo -f nc -t ecmwf copy *.grib tmp.nc
+elif [ -f *.nc ]; then
+   mv *.nc tmp.nc
 else
    echo "No data found! Check your request and/or credentials."
 fi
